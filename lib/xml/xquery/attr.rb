e76617e32799feb8a214c3f9b5e8af8c06cb5ce1
Jens Wille <jens.wille@uni-koeln.de>
2008-03-29 12:44:09 +0000

    <ruby-xquery> lib/xml/xquery/attr.rb: Overwrite #to_s to just give the value (stripped). lib/xml/xquery/version.rb: v0.0.8
    git-svn-id: http://prometheus.rubyforge.org/svn/scratch/ruby-xquery@227 d799ba13-25ec-4c21-81e7-7bafc1e0b839

---
+++ lib/xml/xquery/attr.rb
@@ -0,0 +1,33 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-xquery, mimicking XQuery in Ruby.                       #
+#                                                                             #
+# Copyright (C) 2007-2008 Jens Wille                                          #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-xquery is free software; you can redistribute it and/or modify it      #
+# under the terms of the GNU General Public License as published by the Free  #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
+#                                                                             #
+# ruby-xquery is distributed in the hope that it will be useful, but WITHOUT  #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with ruby-xquery. If not, see <http://www.gnu.org/licenses/>.               #
+#                                                                             #
+###############################################################################
+#++
+
+class XML::Attr
+
+  def to_s(sep = nil)
+    value.strip
+  end
+
+end
