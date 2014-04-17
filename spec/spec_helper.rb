0d7818e001251a1a58aba743e936c69734f27973
Jens Wille <jens.wille@gmail.com>
2014-04-17 17:47:13 +0200

    spec/: Prepare for RSpec 3.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,7 @@ require 'libxml/ext'
 unless Object.const_defined?(:DOC)
   DOC = LibXML::XML::Document.file(File.join(File.dirname(__FILE__), 'hamlet.xml'))
 end
+
+RSpec.configure { |config|
+  config.expect_with(:rspec) { |c| c.syntax = [:should, :expect] }
+}
